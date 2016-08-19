13fafee10cda4f36eb2265ea9bd7cfaedb9057a7
Vincent Trumpff <vincent@stoic.com>
2016-08-19 22:12:51 +0200

    interrupts section

--- src/kernel/hw/interrupts.cc
+++ src/kernel/hw/interrupts.cc
@@ -17,7 +17,7 @@ namespace kernel {
           GPU and therefore cause the GPU to start running code again until
           the ARM is handed control at the end of boot loading
       */
-      extern "C" void __attribute__((naked, interrupt("ABORT"))) reset_vector() {
+      extern "C" void __attribute__((naked, interrupt("ABORT"), section(".text.interrupts"))) reset_vector() {
         DEBUG("reset_vector");
       }
 
@@ -26,7 +26,7 @@ namespace kernel {
           If an undefined intstruction is encountered, the CPU will start
           executing this function. Just trap here as a debug solution.
       */
-      extern "C" void __attribute__((naked, interrupt("UNDEF"))) undefined_instruction_vector() {
+      extern "C" void __attribute__((naked, interrupt("UNDEF"), section(".text.interrupts"))) undefined_instruction_vector() {
         DEBUG("undefined_instruction_vector");
         for(;;);
       }
@@ -36,7 +36,7 @@ namespace kernel {
           The CPU will start executing this function. Just trap here as a debug
           solution.
       */
-      extern "C" void __attribute__((naked, interrupt("SWI"))) software_interrupt_vector() {
+      extern "C" void __attribute__((naked, interrupt("SWI"), section(".text.interrupts"))) software_interrupt_vector() {
         DEBUG("software_interrupt_vector");
         for(;;);
       }
@@ -46,7 +46,7 @@ namespace kernel {
           The CPU will start executing this function. Just trap here as a debug
           solution.
       */
-      extern "C" void __attribute__((naked, interrupt("ABORT"))) prefetch_abort_vector() {
+      extern "C" void __attribute__((naked, interrupt("ABORT"), section(".text.interrupts"))) prefetch_abort_vector() {
         DEBUG("prefetch_abort_vector");
         for(;;);
       }
@@ -56,7 +56,7 @@ namespace kernel {
           The CPU will start executing this function. Just trap here as a debug
           solution.
       */
-      extern "C" void __attribute__((naked, interrupt("ABORT"))) data_abort_vector() {
+      extern "C" void __attribute__((naked, interrupt("ABORT"), section(".text.interrupts"))) data_abort_vector() {
         DEBUG("data_abort_vector");
         for(;;);
       }
@@ -68,7 +68,7 @@ namespace kernel {
           importantly clear the interrupt flag so that the interrupt won't
           immediately put us back into the start of the handler again.
       */
-      extern "C" void __attribute__((naked, interrupt("IRQ"))) interrupt_vector() {
+      extern "C" void __attribute__((naked, interrupt("IRQ"), section(".text.interrupts"))) interrupt_vector() {
         DEBUG("interrupt_vector");
         for(;;);
       }
@@ -94,7 +94,7 @@ namespace kernel {
           empty because the CPU has switched to a fresh set of registers and so has
           not altered the main set of registers.
       */
-      extern "C" void __attribute__((naked, interrupt("FIQ"))) fast_interrupt_vector() {
+      extern "C" void __attribute__((naked, interrupt("FIQ"), section(".text.interrupts"))) fast_interrupt_vector() {
         DEBUG("fast_interrupt_vector");
         for(;;);
       }
@@ -108,29 +108,17 @@ namespace kernel {
        * vector is clearly an error. Also, resetting the Pi will reset VideoCore,
        * and reboot.
        */
-      extern "C" __attribute__ ((naked, aligned(32))) void vectors() {
+      extern "C" __attribute__ ((naked, aligned(32), section(".text.interrupts"))) void vectors() {
         asm volatile(
           "b reset_vector\n" // ARM4_XRQ_RESET
           "b undefined_instruction_vector\n" // ARM4_XRQ_UNDEF
           "b software_interrupt_vector\n" // ARM4_XRQ_SWINT
           "b prefetch_abort_vector\n" // ARM4_XRQ_ABRTP
           "b data_abort_vector\n" // ARM4_XRQ_ABRTD
-          "b bad_exception\n" // ARM4_XRQ_RESV1
+          "b reset_vector\n" // ARM4_XRQ_RESV1
           "b interrupt_vector\n" // ARM4_XRQ_IRQ
           "b fast_interrupt_vector\n" // ARM4_XRQ_FIQ
         );
-      }
-
-      /* Unhandled exceptions - hang the machine */
-      extern "C" __attribute__ ((naked)) void bad_exception() {
-        DEBUG("bad exception");
-        for(;;);
-      }
-
-      /* IRQs flash the OK LED */
-      extern "C" __attribute__ ((interrupt ("IRQ"))) void interrupt_irq() {
-        DEBUG("interrupt irq");
-        for(;;);
       }
 
       void init() {
