39a2f977b109dec28c32d0f9ac6bcb23c222a0eb
Vincent Trumpff <vincent@stoic.com>
2016-08-31 21:33:55 +0200

    list tests

--- src/test/utils-list.cc
+++ src/test/utils-list.cc
@@ -10,9 +10,57 @@
 
 namespace test {
 
+  class node_test : public kernel::utils::list<node_test>::node {
+    int _value;
+
+  public:
+    explicit inline node_test(const int &val)
+     : _value(val) {
+    }
+
+    const int &value() const { return _value; }
+  };
+
   void utils_list() {
     DEBUG("test begin");
 
+    kernel::utils::list<node_test> list;
+
+    node_test val1(1);
+    node_test val2(2);
+    node_test val3(3);
+
+    ASSERT(list.empty());
+    ASSERT(list.count() == 0);
+
+    list.add(&val1);
+
+    ASSERT(!list.empty());
+    ASSERT(list.count() == 1);
+
+    list.remove(&val1);
+
+    ASSERT(list.empty());
+    ASSERT(list.count() == 0);
+
+    list.add(&val1);
+    list.add(&val2);
+    list.add(&val3);
+
+    ASSERT(!list.empty());
+    ASSERT(list.count() == 3);
+
+    int i=0;
+    for(auto node : list) {
+      ASSERT(node->value() == ++i);
+    }
+
+    list.remove(&val2);
+    list.remove(&val3);
+    list.remove(&val1);
+
+    ASSERT(list.empty());
+    ASSERT(list.count() == 0);
 
     DEBUG("test end");
   }
