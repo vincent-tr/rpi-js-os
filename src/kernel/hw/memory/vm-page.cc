79fe6557b3bd8511c4bc85e3c0c74c5779f22eb3
Vincent Trumpff <vincent@stoic.com>
2016-09-11 21:27:46 +0200

    added table desc WIP

--- src/kernel/hw/memory/vm-page.cc
+++ src/kernel/hw/memory/vm-page.cc
@@ -12,11 +12,24 @@ namespace kernel {
   namespace hw {
     namespace memory {
 
-      struct vm_region_info {
+      // http://infocenter.arm.com/help/topic/com.arm.doc.ddi0198e/Babejahc.html
 
+      // [31:10] These bits form the base for referencing the second-level descriptor (the coarse page table index for the entry is derived from the MVA)
+      // [9]     Always written as 0
+      // [8:5]   These bits specify one of the 16 possible domains, held in the domain access control registers, that contain the primary access controls
+      // [4]     Always written as 1
+      // [3:2]   Always written as 0
+      // [1:0]   These bits must be 01 to indicate a coarse page table descriptor
+
+      struct coarse_page_table_descriptor {
       };
 
-      struct vm_page_info {
+      // [31:12] These bits form the corresponding bits of the physical address.
+      // [11:4]  Access permission bits. Domain access control and Fault checking sequence show how to interpret the access permission bits.
+      // [3:2]   These bits, C and B, indicate whether the area of memory mapped by this page is treated as write-back cacheable, write-through cacheable, noncached buffered, or noncached nonbuffered.
+      // [1:0]   These bits indicate the page size and validity and are interpreted as shown in Table 3.8.
+
+      struct small_page_table_descriptor {
 
       };
 
