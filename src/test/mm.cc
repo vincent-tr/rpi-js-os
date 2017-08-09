491f97c9e2eaf6556deac56a09694173c1212a65
Vincent Trumpff <vincent@stoic.com>
2017-08-09 23:38:46 +0300

    add total_size

--- src/test/mm.cc
+++ src/test/mm.cc
@@ -31,15 +31,18 @@ namespace test {
     kernel::mm::region *regions[regions_count];
     const uint32_t &page_size = kernel::platform::get().page_size();
 
+    uint32_t total_size = 0;
     for(uint32_t i=0; i<regions_count; ++i) {
-      kernel::mm::region *r = kernel::mm::region::create((i+1) * page_size, kernel::mm::protection{1,1}, "test");
+      uint32_t size = (i+1) * page_size;
+      kernel::mm::region *r = kernel::mm::region::create(size, kernel::mm::protection{1,1}, "test");
+      total_size += size;
       regions[i] = r;
       for(uint32_t addr = r->address(); addr < r->address_end(); addr += sizeof(addr)) {
         *reinterpret_cast<uint32_t *>(addr) = r->address();
       }
     }
 
-    DEBUG(regions_count << " regions created");
+    DEBUG(regions_count << " regions created (total_size=" << total_size << ")");
     print_regions();
 
     for(uint32_t i=0; i<regions_count; ++i) {
