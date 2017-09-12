4aae6db9d5c1a44cc9c856f2af399f1213038b36
Vincent Trumpff <vincent@stoic.com>
2017-09-12 17:51:38 +0200

    WIP libcxxrt integration

--- common.mk
+++ common.mk
@@ -4,13 +4,16 @@ CC = $(TOOLCHAIN_PREFIX)gcc
 CXX = $(TOOLCHAIN_PREFIX)gcc
 LINK = $(TOOLCHAIN_PREFIX)gcc
 OBJCOPY = $(TOOLCHAIN_PREFIX)objcopy
+AR = $(TOOLCHAIN_PREFIX)ar
 
 COMMON_FLAGS = -mcpu=arm1176jzf-s -fpic -ffreestanding $(DEFINES)
 COMMON_ASFLAGS = $(COMMON_FLAGS)
 COMMON_CFLAGS = -std=gnu99 -O2 -Wall -Wextra $(COMMON_FLAGS)
-COMMON_CXXFLAGS = -std=gnu++14 -O2 -Wall -Wextra -pedantic -fno-exceptions -fno-use-cxa-atexit -fno-threadsafe-statics $(COMMON_FLAGS)
+COMMON_CXXFLAGS = -std=gnu++14 -O2 -Wall -Wextra -pedantic -fno-use-cxa-atexit -fno-threadsafe-statics $(COMMON_FLAGS)
 
 NEWLIB_VERSION = 2.5.0
+LIBCXX_VERSION = 5.0
+LIBCXXRT_VERSION = 2017.08
 
 MKDIR = mkdir
 FIND = find
